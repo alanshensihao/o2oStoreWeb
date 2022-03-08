0c057c3df036d9987014fe747ee34c019720409a
alanshensihao <alanshensihao@gmail.com>
2022-03-08 16:51:35 -0500

    logger:configuration for AreaController and test complete

--- o2o/src/main/java/com/imooc/o2o/web/superadmin/AreaController.java
+++ o2o/src/main/java/com/imooc/o2o/web/superadmin/AreaController.java
@@ -8,15 +8,20 @@ import java.util.Map;
 import com.imooc.o2o.entity.Area;
 import com.imooc.o2o.service.AreaService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
     // when areaService used, inject areaService impl
     @Autowired
     private AreaService areaService;
@@ -26,6 +31,8 @@ public class AreaController {
     @RequestMapping(value="/listarea", method = RequestMethod.GET)
     @ResponseBody
     private Map<String, Object> listArea() {
+        logger.info("===start===");
+        long startTime = System.currentTimeMillis();
         // non duplicate areas
         Map<String, Object> modelMap = new HashMap<String, Object>();
         // create area list
@@ -40,6 +47,10 @@ public class AreaController {
             modelMap.put("success",false);
             modelMap.put("errMsg",e.toString());
         }
+        logger.error("test error!");
+        long endTime = System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]",endTime-startTime);
+        logger.info("===end===");
         return modelMap;
     }
 }
